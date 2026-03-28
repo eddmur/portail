d0846a1c55ae12229ac9e103e1aeb286e8a6ac86
eddmur <samuel_bellaiche@hotmail.fr>
2026-03-28 23:09:49 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+from pylgate import generate_token
+from pylgate.types import TokenType
+import requests
+
+app = Flask(__name__)
+
+SESSION_TOKEN = bytes.fromhex("149e9b790f31c6f68a7122bf2416b697")
+PHONE         = 33600000000
+DEVICE        = "4G300208179"
+
+@app.route('/open')
+def open_gate():
+    token = generate_token(SESSION_TOKEN, PHONE, TokenType.PRIMARY)
+    r = requests.get(
+        f'https://api1.pal-es.com/v1/bt/device/{DEVICE}/open-gate?outputNum=1',
+        headers={'User-Agent': 'okhttp/4.9.3', 'X-Bt-Token': token}
+    )
+    return jsonify(r.json())
